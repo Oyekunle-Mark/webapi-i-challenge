87adaa5442632ce9f1f16e2a23fc1631aa713bf1
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-08 17:16:18 +0100

    The container app and state component

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-import SampleComponent from './components/SampleComponent';
+import UserContext from './userContext';
+import Users from './components/Users';
+import UserForm from './components/UserForm';
 
-const App = () => (
-  <div>
-    <h1>I am the container</h1>
-    <SampleComponent />
-  </div>
-);
+class App extends Component {
+  state = {
+    users: [],
+    updateUsers: user =>
+      this.setState(prevState => ({
+        users: [...prevState.users, user],
+      })),
+    deleteUser: id =>
+      this.setState(prevState => ({
+        users: prevState.users.filter(user => user.id !== id),
+      })),
+  };
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/api/users')
+      .then(res =>
+        this.setState({
+          users: res.data,
+        }),
+      )
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div>
+        <UserContext.Provider value={this.state}>
+          <UserForm />
+          <Users />
+        </UserContext.Provider>
+      </div>
+    );
+  }
+}
 
 export default App;
