721742703a07893a59ff68c3f023859f5781e7fa
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-08 18:25:36 +0100

    Created  the method for user update

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -8,13 +8,40 @@ import UserForm from './components/UserForm';
 class App extends Component {
   state = {
     users: [],
-    updateUsers: user =>
+    currentlyEdited: {
+      name: '',
+      bio: '',
+    },
+    isEditing: '',
+    createUser: user =>
       this.setState(prevState => ({
         users: [...prevState.users, user],
       })),
     deleteUser: id =>
       this.setState(prevState => ({
         users: prevState.users.filter(user => user.id !== id),
+      })),
+    setEditedUser: (id, user) =>
+      this.setState({
+        isEditing: id,
+        currentlyEdited: user,
+      }),
+    updateCurrentlyEdited: user =>
+      this.setState({
+        currentlyEdited: user,
+      }),
+    editUser: id =>
+      this.setState(prevState => ({
+        users: prevState.users.map(user => {
+          if (user.id === id) return { id, ...prevState.currentlyEdited };
+
+          return user;
+        }),
+        isEditing: '',
+        currentlyEdited: {
+          name: '',
+          bio: '',
+        },
       })),
   };
 
