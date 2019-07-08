d76c14c6f52d746663fdbfe6c5cfded30584f931
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-08 13:34:19 +0100

    The endpoint to create a user

--- index.js
+++ index.js
@@ -12,7 +12,29 @@ server.get('/api/users', (req, res) => {
     .catch(() =>
       res
         .status(500)
-        .end({ error: 'The users information could not be retrieved.' }),
+        .json({ error: 'The users information could not be retrieved.' }),
+    );
+});
+
+server.post('/api/users', (req, res) => {
+  const { name, bio } = req.body;
+  if (!name || !bio)
+    return res
+      .status(400)
+      .json({ errorMessage: 'Please provide name and bio for the user.' });
+
+  User.insert({ name, bio })
+    .then(user =>
+      res.status(201).json({
+        id: user.id,
+        name,
+        bio,
+      }),
+    )
+    .catch(() =>
+      res.status(500).json({
+        error: 'There was an error while saving the user to the database',
+      }),
     );
 });
 
