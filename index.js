d72bf2663d43301b210b7480f64df0afca99135b
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-08 13:07:18 +0100

    Setup the express server

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// implement your API here
+const express = require('express');
+
+const server = express();
+const PORT = 5000;
+
+server.use(express.json());
+
+server.use((req, res) => {
+  res.status(404).json({
+    status: 404,
+    message: 'Please check the URL and try again.',
+  });
+});
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
