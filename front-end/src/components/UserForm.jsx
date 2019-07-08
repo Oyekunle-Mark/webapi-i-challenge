3b565e2b0bcc4df42cf04e78443fa0846a6d7319
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-08 18:26:05 +0100

    Provided support for user edit from the UserForm component

--- front-end/src/components/UserForm.jsx
+++ front-end/src/components/UserForm.jsx
@@ -4,27 +4,50 @@ import axios from 'axios';
 import userContext from '../userContext';
 
 export default () => {
+  const {
+    createUser,
+    isEditing,
+    currentlyEdited,
+    updateCurrentlyEdited,
+    editUser,
+  } = useContext(userContext);
+
   const [formInputs, updateFormInput] = useState({
     name: '',
     bio: '',
   });
 
-  const { updateUsers } = useContext(userContext);
-
   const updateForm = e => {
-    updateFormInput({
-      ...formInputs,
-      [e.target.name]: e.target.value,
-    });
+    if (isEditing) {
+      updateCurrentlyEdited({
+        ...currentlyEdited,
+        [e.target.name]: e.target.value,
+      });
+    } else {
+      updateFormInput({
+        ...formInputs,
+        [e.target.name]: e.target.value,
+      });
+    }
   };
 
   const handleSubmit = e => {
     e.preventDefault();
 
-    axios
-      .post('http://localhost:5000/api/users', formInputs)
-      .then(res => updateUsers(res.data))
-      .catch(err => console.log(err));
+    if (isEditing) {
+      axios
+        .put(
+          `http://localhost:5000/api/users/${Number(isEditing)}`,
+          currentlyEdited,
+        )
+        .then(() => editUser(Number(isEditing)))
+        .catch(err => console.log(err));
+    } else {
+      axios
+        .post('http://localhost:5000/api/users', formInputs)
+        .then(res => createUser(res.data))
+        .catch(err => console.log(err));
+    }
   };
 
   return (
@@ -32,18 +55,18 @@ export default () => {
       <input
         type="text"
         name="name"
-        value={formInputs.name}
+        value={isEditing ? currentlyEdited.name : formInputs.name}
         onChange={updateForm}
         placeholder="enter name"
       />
       <input
         type="text"
         name="bio"
-        value={formInputs.value}
+        value={isEditing ? currentlyEdited.bio : formInputs.bio}
         onChange={updateForm}
         placeholder="user's bio"
       />
-      <button type="submit">Add Friend</button>
+      <button type="submit">{isEditing ? 'Update' : 'Add Friend'}</button>
     </form>
   );
 };
