9be0374df9f2e6ac32ee77bffe81439cb797740d
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-08 13:51:56 +0100

    Created the delete user endpoint

--- index.js
+++ index.js
@@ -57,6 +57,23 @@ server.get('/api/users/:id', (req, res) => {
     );
 });
 
+server.delete('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+
+  User.remove(id)
+    .then(user => {
+      if (!user)
+        return res
+          .status(404)
+          .json({ message: 'The user with the specified ID does not exist.' });
+
+      res.status(200).json(user);
+    })
+    .catch(() =>
+      res.status(500).json({ error: 'The user could not be removed' }),
+    );
+});
+
 server.use((req, res) => {
   res.status(404).json({
     status: 404,
